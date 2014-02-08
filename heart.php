666942948f54976c94a31e6d3e02173c1684c562
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-02-08 23:16:57 +0300

    Update heart.php

--- heart.php
+++ heart.php
@@ -1,4 +1,11 @@
 <?php
+require_once("memory.php");
 $datetime = getdate();
 echo $datetime['hours'].":".$datetime['minutes'].":".$datetime['seconds'];
+$myMemory = ABMemory('mysql.serversfree.com', 'u366911672_ab', 'u366911672_ab', 'parole');
+try {
+  $myMemory -> connectMySQL();
+} catch (ABMemoryException $e) {
+  echo $e -> getMessage();
+}
 ?>
