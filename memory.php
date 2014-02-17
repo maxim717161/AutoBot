4fd152770eecc7bb4c1e5dc1bc736b2a2505c555
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-02-17 20:04:16 +0300

    Update memory.php

--- memory.php
+++ memory.php
@@ -17,7 +17,7 @@ class ABMemory {
   protected $CONNECT_FAILED = "Unable to connect to DB: ";
   protected $DB_FAILED = "Unable to select DB: ";
   protected $QUERY_FAILED = "Could not successfully run query from DB: ";
-  protected $Tables = array("ticks" => array("UNIQUE time" => "DATETIME", "vAsk" => "DOUBLE", "vBid"=>"DOUBLE", "pOpen" => "DOUBLE","pHigh" => "DOUBLE", "pLow" => "DOUBLE", "pClose" => "DOUBLE"), "users"=> array("UNIQUE email"=>"CHAR(255)", "pass"=>"CHAR(255)", "isVer"=>"BIT","lastEnter"=>"DATETIME"));
+  protected $Tables = array("ticks" => array("time" => "DATETIME", "vAsk" => "DOUBLE", "vBid"=>"DOUBLE", "pOpen" => "DOUBLE","pHigh" => "DOUBLE", "pLow" => "DOUBLE", "pClose" => "DOUBLE"), "users"=> array("email"=>"CHAR(255)", "pass"=>"CHAR(255)", "isVer"=>"BIT","lastEnter"=>"DATETIME"));
   
   public function __construct ( $db_host , $db_name , $db_user, $db_pass) {
     $this ->db_host = $db_host;
