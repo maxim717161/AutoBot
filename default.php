6e8dac065da0a320273e1c0c60851db45de8ff7a
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-02-20 17:34:47 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
 <head>
 <meta charset= "utf-8" >
-<title> AutoBot &Alpha; &amp; &Omega; 0.0.1 </title>
+<title> AutoBot &Alpha;&amp;&Omega; v0.0.1 </title>
 <style>
-table .m {
-border-collapse : collapse ; /* Убираем двойные границы */
+.m {
+border: 0 ; /* Убираем двойные границы */
 width : 100% ; /* Ширина таблицы */
 height: 100% ;
 border-spacing: 0 ; /* Расстояние между ячеек */
-background : #f0f0f0 ; /* Цвет фона */
+background : #F5FFFA ; /* Цвет фона */
 }
-td .top {
-border : 1 px solid #6A3E14 ; /* Параметры рамки */
-padding: 4 px; /* Поля в ячейках */
+.top {
+text-align: center;
 }
-td .mid {
-background : #808990 ; /* Цвет фона */
-color : #fff ; /* Цвет текст */
+.mid {
+text-align: center;
+border: 1px solid black
 }
-td .bot {
-background : #CFD6D3 ; /* Цвет фона */
+.bot {
+text-align: center;
+background : silver ; /* Цвет фона */
 }
+p {text-align: justify;}
 .r {color: red;}
 .g {color: green;}
 .s {color: grey;}
-p :first-letter {
-color : red ; /* Красный цвет первой буквы */
-font-size : 200% ; /* Размер шрифта первого символа */
-}
-p .f {
+.f {
 color : black ; /* Красный цвет текста */
 font-style : italic ; /* Курсивное начертание */
 font-weight : bold ; /* Жирное начертание */
@@ -36,8 +33,8 @@ font-weight : bold ; /* Жирное начертание */
 </style>
 </head>
 <body>
-<table>
-<tr><td class="top"> <h1><span class="r">Auto</span>Bot &Alpha; &amp; &Omega;</h1> </td></tr>
+<table class="m">
+<tr><td class="top"> <h1><span class="r">Auto</span>Bot <span class="g">&Alpha;</span>&amp;<span class="g">&Omega;</span></h1> </td></tr>
 <tr><td class="mid"> <p class="f">Евангелие от Матфея</p>
 <p><span class="r">25:26</span> Господин же его сказал ему в ответ: лукавый раб и ленивый! ты знал, что я жну, где не сеял, и собираю, где не рассыпал;</p>
 
