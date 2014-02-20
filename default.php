8344bc4c06177788214ffecbd76436cac6ff8c80
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-02-20 17:06:17 +0300

    Create default.php

---
+++ default.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<head>
+<meta charset= "utf-8" >
+<title> AutoBot &Alpha; &amp; &Omega; 0.0.1 </title>
+<style>
+table .m {
+border-collapse : collapse ; /* Убираем двойные границы */
+width : 100% ; /* Ширина таблицы */
+height: 100% ;
+border-spacing: 0 ; /* Расстояние между ячеек */
+background : #f0f0f0 ; /* Цвет фона */
+}
+td .top {
+border : 1 px solid #6A3E14 ; /* Параметры рамки */
+padding: 4 px; /* Поля в ячейках */
+}
+td .mid {
+background : #808990 ; /* Цвет фона */
+color : #fff ; /* Цвет текст */
+}
+td .bot {
+background : #CFD6D3 ; /* Цвет фона */
+}
+.r {color: red;}
+.g {color: green;}
+.s {color: grey;}
+p :first-letter {
+color : red ; /* Красный цвет первой буквы */
+font-size : 200% ; /* Размер шрифта первого символа */
+}
+p .f {
+color : black ; /* Красный цвет текста */
+font-style : italic ; /* Курсивное начертание */
+font-weight : bold ; /* Жирное начертание */
+}
+</style>
+</head>
+<body>
+<table>
+<tr><td class="top"> <h1><span class="r">Auto</span>Bot &Alpha; &amp; &Omega;</h1> </td></tr>
+<tr><td class="mid"> <p class="f">Евангелие от Матфея</p>
+<p><span class="r">25:26</span> Господин же его сказал ему в ответ: лукавый раб и ленивый! ты знал, что я жну, где не сеял, и собираю, где не рассыпал;</p>
+
+<p><span class="r">25:27</span> посему надлежало тебе отдать серебро мое торгующим, и я, придя, получил бы мое с прибылью;</p> </td>
+<tr><td class="mid"> 1999 </td></tr>
+<tr><td class="bot"> <p>&copy; 2014 <span class="r">Auto</span>Bot <span class="g" >&Alpha;&amp;&Omega;</span>,
+<span class="s">maxim.bagno</span> </p></td></tr>
+</table>
+</body>
+</html>
