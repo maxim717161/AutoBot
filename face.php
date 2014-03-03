b04f8a0d83aa4ec9964f3854bb6c252ecc2a2e24
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-03 18:41:12 +0300

    Create face.php

---
+++ face.php
@@ -0,0 +1,8 @@
+<form>
+<table>
+<tr><td>Е-майл:</td>
+<td><input type="text"/></td></tr>
+<tr><td>Пароль:</td>
+<td><input type="password"/></td></tr>
+<tr><td colspan=2><input type="button"></td></tr>
+</table></form>
