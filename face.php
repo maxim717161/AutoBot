870425002e46c72c96c289125f25f703abd3e43e
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 21:19:03 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
-<head>
-<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
-<title>qwerty</title>
-<link rel="icon" href="/favicon.ico?2" type="image/x-icon"/>
-<link rel="shortcut icon" href="/favicon.ico?2" type="image/x-icon"/>
-</head>
-<body>
+
+<?php require("header.php"); ?>
+<tr><td>
 <form>
 <table>
 <tr><td>Е-майл:</td>
@@ -15,5 +9,5 @@
 <td><input type="password"/></td></tr>
 <tr><td colspan=2><input type="button"></td></tr>
 </table></form>
-</body>
-</html>
+</td></tr>
+<?php require("footer.php"); ?>
