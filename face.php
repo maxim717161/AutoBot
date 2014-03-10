c5580442b4c41324180d6059d633eb6c09540417
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 15:25:51 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
+<body>
 <form>
 <table>
 <tr><td>Е-майл:</td>
@@ -6,3 +9,5 @@
 <td><input type="password"/></td></tr>
 <tr><td colspan=2><input type="button"></td></tr>
 </table></form>
+</body>
+</html>
