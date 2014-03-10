cdc094f8824c8ca2bb6368a7bd820a001891bb2f
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 19:46:43 +0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
+<head>
+<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<title> AutoBot &Alpha;&amp;&Omega; v0.0.1</title>
+</head>
+<body>
+<table style="height:100%;" border="0" width="100%" cellpadding="0" cellspacing="0">
+<tr><td valign="top" align="center"> 
+<h1><span style="color:red;">Auto</span>Bot <span style="colr:green;">&Alpha;&amp;&Omega;</span></h1>
+</td></tr>
