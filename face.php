8718a5913b4782a673cfe8dc4e44c2abc64ec220
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 15:35:59 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,5 +1,11 @@
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
+<head>
+<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
+<title>qwerty</title>
+<link rel="icon" href="/favicon.ico?2" type="image/x-icon"/>
+<link rel="shortcut icon" href="/favicon.ico?2" type="image/x-icon"/>
+</head>
 <body>
 <form>
 <table>
