d1b8857bb59e93ab069756e46df720031ee32651
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 20:05:44 +0300

    Update default.php

--- default.php
+++ default.php
@@ -1,20 +1,6 @@
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
-<head>
-<meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
-<link rel="icon" href="favicon.ico" type="image/x-icon"/>
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
-<title> AutoBot &Alpha;&amp;&Omega; v0.0.1</title>
-</head>
-<body>
-<table style="height:100%;" border="0" width="100%" cellpadding="0" cellspacing="0">
-<tr><td valign="top" align="center"> <h1><span style="color:red;">Auto</span>Bot <span style="colr:green;">&Alpha;&amp;&Omega;</span></h1> </td></tr>
+<?php require("header.php"); ?>
 <tr><td valign="top" align="justify"> <span style="color:red;"><b>25:27</b></span> посему надлежало тебе отдать серебро мое торгующим, и я, придя, получил бы мое с прибылью; <b><i>(Евангелие от Матфея)</i></b></td>
 <tr><td valign="middle" align="center"> 
 <br/><img src="http://dc-charts.com/fw/250_btce_btc_usd_d1_1.png" alt="BTC-E bitcoin price chart" title="BTC-E BTC/USD price chart - http://dc-charts.com" width="250" height="160" border="0" /><br/><br/>
 <a style="padding-left:40px; padding-right:40px; background-color:black; color:white; border:3px double white;" href="face.php"><b> Вход </b></a><br/><br/> </td></tr>
-<tr><td valign="bottom" align="center">&copy; 2014 <span style="color:red;">Auto</span>Bot <span style="color:green;" >&Alpha;&amp;&Omega;</span>,
-<span style="color:silver;">maxim.bagno</span></td></tr>
-</table>
-</body>
-</html>
+<?php require("footer.php"); ?>
