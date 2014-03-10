f9324df3dc7d3dcba1deea5ddfe338f312e66ec9
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 16:13:38 +0300

    Update default.php

--- default.php
+++ default.php
@@ -2,56 +2,20 @@
 <html xmlns="http://www.w3.org/1999/xhtml" class="no-js">
 <head>
 <meta http-equiv="Content-Type" content="application/xhtml+xml; charset=UTF-8"/>
-<link rel="icon" href="/favicon.ico" type="image/x-icon"/>
-<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"/>
-<title> AutoBot &Alpha;&amp;&Omega; v0.0.1 </title>
-<style>
-.m {
-border: 0 ; /* Убираем двойные границы */
-width : 100% ; /* Ширина таблицы */
-height: 100% ;
-border-spacing: 0 ; /* Расстояние между ячеек */
-background : white; /* Цвет фона */
-}
-.top {
-vertical-align: top;
-text-align: center;
-}
-.mid {
-vertical-align: center;
-text-align: center;
-}
-.bot {
-vertical-align: bottom;
-text-align: center;
-background : white ; /* Цвет фона */
-}
-.k {
-font-weight:bold;
-text-align: center;
-width: 30%;
-border: 3px double white;
-padding: 10px;
-background: black;
-color: white;
-}
-.p {text-align: justify;}
-.r {color: red;}
-.g {color: green;}
-.s {color: silver;}
-.f {
-text-align: right;
-font-style : italic;}
-</style>
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+<title> AutoBot &Alpha;&amp;&Omega; v0.0.1</title>
 </head>
 <body>
 <table class="m">
 <tr><td class="top"> <h1><span class="r">Auto</span>Bot <span class="g">&Alpha;&amp;&Omega;</span></h1> </td></tr>
 <tr><td class="mid">
 <p class="p"><span class="r">25:27</span> посему надлежало тебе отдать серебро мое торгующим, и я, придя,
-получил бы мое с прибылью;</p> 
-<p class="f">Евангелие от Матфея</p></td>
-<tr><td class="mid"> <a class="k" href="heart.php"> Вход </a><br/> </td></tr>
+получил бы мое с прибылью; <i>(Евангелие от Матфея)</i></p></td>
+<tr><td class="mid"> 
+<img src="http://dc-charts.com/fw/250_btce_btc_usd_d1_1.png" alt="BTC-E bitcoin price chart" title="BTC-E BTC/USD price chart - http://dc-charts.com" width="250" height="160" border="0" /><br>
+<img src="http://dc-charts.com/fw/250_btcchina_btc_cny_d1_1.png" alt="BTCChina bitcoin price chart" title="BTCChina BTC/CNY price chart - http://dc-charts.com" width="250" height="160" border="0" /><br>
+<a class="k" href="heart.php"> Вход </a><br/> </td></tr>
 <tr><td class="bot"><p>&copy; 2014 <span class="r">Auto</span>Bot <span class="g" >&Alpha;&amp;&Omega;</span>,
 <span class="s">maxim.bagno</span></p></td></tr>
 </table>
