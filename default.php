c5b0f265b2fcbab0466576ea85f765bb50764b9a
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 18:19:21 +0300

    Update default.php

--- default.php
+++ default.php
@@ -12,7 +12,7 @@
 <tr><td valign="top" align="justify"> <span style="color:red;"><b>25:27</b></span> посему надлежало тебе отдать серебро мое торгующим, и я, придя, получил бы мое с прибылью; <b><i>(Евангелие от Матфея)</i></b></td>
 <tr><td valign="middle" align="center"> 
 <br/><img src="http://dc-charts.com/fw/250_btce_btc_usd_d1_1.png" alt="BTC-E bitcoin price chart" title="BTC-E BTC/USD price chart - http://dc-charts.com" width="250" height="160" border="0" /><br/><br/>
-<a style="padding:20px; margin:20px; background-color:black; color:white; border:3px double white;" href="face.php"> Вход </a><br/><br/> </td></tr>
+<a style="padding:20px; margin:5px 30px 5px 30px; background-color:black; color:white; border:3px double white;" href="face.php"><b> Вход </b></a><br/><br/> </td></tr>
 <tr><td valign="bottom" align="center">&copy; 2014 <span style="color:red;">Auto</span>Bot <span style="color:green;" >&Alpha;&amp;&Omega;</span>,
 <span style="color:silver;">maxim.bagno</span></td></tr>
 </table>
