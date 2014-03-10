b44deb1f3fb53367610b5c37a271f72ec193d934
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-10 19:49:05 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,6 @@
+<tr><td valign="bottom" align="center">
+&copy; 2014 <span style="color:red;">Auto</span>Bot <span style="color:green;" >&Alpha;&amp;&Omega;</span>,
+<span style="color:silver;">maxim.bagno</span></td></tr>
+</table>
+</body>
+</html>
