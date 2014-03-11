80fe754c6510bba023f6b25cdf5d9ccbcaf9dae6
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-11 23:12:15 +0300

    Update face.php

--- face.php
+++ face.php
@@ -4,13 +4,13 @@
 <br/>
 <form  id="login" action ="face.php" method= "post">
 <table border="0">
-<tr><td colspan="3" align="center"><h6>Вход / Регистрация</h6></td></tr>
+<tr><td colspan="3" align="center"><b>Вход / Регистрация</b></td></tr>
 <tr><td>Е-майл:</td>
 <td colspan="2"><input type="text" id="email"/></td></tr>
 <tr><td>Пароль:</td>
 <td colspan="2"><input type="password" id="parole"/></td></tr>
 <tr><td>Новый?</td>
-<td width="10"><input type="checkbox" id="isreg" onchange="document.getElementById( 'button' ).value='Регистрация'"/></td>
+<td width="10"><input type="checkbox" id="isreg" onchange="if(document.getElementById( 'isreg' ).value){document.getElementById( 'button' ).value='Регистрция'}else{document.getElementById( 'button' ).value='Войти'}"/></td>
 <td align="center"><input type="submit" id="button" value="Войти"/></td></tr>
 </table></form>
 <br/>
