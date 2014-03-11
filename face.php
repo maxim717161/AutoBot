18b2825f01734d91d3b62b1e56bcc19de310f7f8
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-11 20:17:01 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,14 +1,18 @@
 <?php require("header.php"); ?>
 <tr><td valign="top" align="justify"> <span style="color:red;"><b>25:27</b></span> посему надлежало тебе отдать серебро мое торгующим, и я, придя, получил бы мое с прибылью; <b><i>(Евангелие от Матфея)</i></b></td>
 <tr><td valign="top" align="center">
+<br/>
 <form  id="login" action ="face.php" method= "post">
-<table>
+<table border="1">
+<tr><td colspan="3" align="center"><h6>Вход / Регистрация</h6></td></tr>
 <tr><td>Е-майл:</td>
-<td><input type="text" id="email"/></td></tr>
+<td colspan="2"><input type="text" id="email"/></td></tr>
 <tr><td>Пароль:</td>
-<td><input type="password" id="parole"/></td></tr>
-<tr><td><input type="checkbox" id="isreg" value="" onchange=""/></td>
-<td valign="middle" align="center"><input type="submit" id="buton" value="Войти"/></td></tr>
+<td colspan="2"><input type="password" id="parole"/></td></tr>
+<tr><td>Новый?</td>
+<td width="10"><input type="checkbox" id="isreg" value="" onchange="document.getElementById( 'button' ).value='Регистрация'"/></td>
+<td align="center"><input type="submit" id="button" value="Войти"/></td></tr>
 </table></form>
+<br/>
 </td></tr>
 <?php require("footer.php"); ?>
