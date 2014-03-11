cd8d09f68f2b3ca339b78799fa0eb1c29360ba34
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-11 18:26:55 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,13 +1,13 @@
-
 <?php require("header.php"); ?>
 <tr><td>
-<form>
+<form  id="login" action ="face.php" method= "post">
 <table>
 <tr><td>Е-майл:</td>
-<td><input type="text"/></td></tr>
+<td><input type="text" id="email"/></td></tr>
 <tr><td>Пароль:</td>
-<td><input type="password"/></td></tr>
-<tr><td colspan=2><input type="button"></td></tr>
+<td><input type="password" id="parole"/></td></tr>
+<tr><td><input type="checkbox" id="isreg" value="Регистрация"/></td>
+<td><input type="submit" id="buton" value="Войти"/></td></tr>
 </table></form>
 </td></tr>
 <?php require("footer.php"); ?>
