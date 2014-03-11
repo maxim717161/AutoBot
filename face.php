038d57b1d3a2986f77920335cba7ff45bdae520d
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-11 19:22:10 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,13 +1,14 @@
 <?php require("header.php"); ?>
-<tr><td>
+<tr><td valign="top" align="justify"> <span style="color:red;"><b>25:27</b></span> посему надлежало тебе отдать серебро мое торгующим, и я, придя, получил бы мое с прибылью; <b><i>(Евангелие от Матфея)</i></b></td>
+<tr><td valign="top" align="center">
 <form  id="login" action ="face.php" method= "post">
 <table>
 <tr><td>Е-майл:</td>
 <td><input type="text" id="email"/></td></tr>
 <tr><td>Пароль:</td>
 <td><input type="password" id="parole"/></td></tr>
-<tr><td><input type="checkbox" id="isreg" value="Регистрация"/></td>
-<td><input type="submit" id="buton" value="Войти"/></td></tr>
+<tr><td><input type="checkbox" id="isreg" value="" onchange=""/></td>
+<td valign="middle" align="center"><input type="submit" id="buton" value="Войти"/></td></tr>
 </table></form>
 </td></tr>
 <?php require("footer.php"); ?>
