75f8798f43b730d9b77664e90915b7f70c72073c
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-12 17:12:45 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,15 +1,38 @@
-<?php require("header.php"); ?>
+<?php
+$pageName = " :: Enter";
+if(isset($_POST['button'])) {
+  $isError = false;
+  $erStr = "";
+  if(empty($_POST['email'])) {
+    $isError = true;
+    $erStr .= " Введите е-майл.";
+  } elseif(!preg_match ("/^[a-zA-Z0-9_\.\-]+@([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,6}$/", $_POST['email'])) {
+    $isError = true;
+    $erStr .= " Неверный формат е-майл.";
+  }
+  if(empty($_POST['parole'])) {
+    $isError = true;
+    $erStr .= " Введите пароль.";
+  } elseif(!preg_match ("/\A(\w){8,255}\Z/", $_POST['parole'])) {
+    $isError = true;
+    $erStr .= " Неверный формат пароля. Введите от 8 до 255 символов.";
+  }
+  if(!$isError) {
+    
+  } else {
+    $pageName = " :: Re-Enter";
+  }
+}
+
+require("header.php"); 
+?>
 <tr><td valign="top" align="justify"> <span style="color:red;"><b>13:12</b></span> Теперь мы видим как бы сквозь тусклое стекло, гадательно, тогда же лицем к лицу; теперь знаю я отчасти, а тогда позна'ю, подобно как я познан. <b><i>(Первое послание к Коринфянам святого апостола Павла)</i></b></td>
 <tr><td valign="top" align="center">
-<?php 
-//if(isset($_POST['button'])){
-  echo $_POST['login'].$_POST['email'].$_POST['parole'].$_POST['button'].$_POST['isreg'];
-//}
-?>
 <br/>
 <form  id="login" name="login" action ="face.php" method= "post">
 <table border="0">
 <tr><td colspan="3" align="center"><b>Вход / Регистрация</b></td></tr>
+<?php if(isset($isError)&&$isError) echo "<tr><td colspan='3' align='center' style='border:1px solid red;'><span style='color:red;'>$erStr</span></td></tr>"; ?>
 <tr><td>Е-майл:</td>
 <td colspan="2"><input type="text" id="email" name="email" maxlenght="255"/></td></tr>
 <tr><td>Пароль:</td>
