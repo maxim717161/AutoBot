e7e001f98c53b74c174baa9057a70ac4c7584087
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-12 13:27:44 +0300

    Update face.php

--- face.php
+++ face.php
@@ -1,14 +1,19 @@
 <?php require("header.php"); ?>
 <tr><td valign="top" align="justify"> <span style="color:red;"><b>13:12</b></span> Теперь мы видим как бы сквозь тусклое стекло, гадательно, тогда же лицем к лицу; теперь знаю я отчасти, а тогда позна'ю, подобно как я познан. <b><i>(Первое послание к Коринфянам святого апостола Павла)</i></b></td>
 <tr><td valign="top" align="center">
+<?php 
+if(isset($_POST['button'])){
+  print_r($_POST);
+}
+?>
 <br/>
 <form  id="login" action ="face.php" method= "post">
 <table border="0">
 <tr><td colspan="3" align="center"><b>Вход / Регистрация</b></td></tr>
 <tr><td>Е-майл:</td>
-<td colspan="2"><input type="text" id="email"/></td></tr>
+<td colspan="2"><input type="text" id="email" maxlenght="255"/></td></tr>
 <tr><td>Пароль:</td>
-<td colspan="2"><input type="password" id="parole"/></td></tr>
+<td colspan="2"><input type="password" id="parole" maxlenght="255"/></td></tr>
 <tr><td>Новый?</td>
 <td width="10"><input type="checkbox" id="isreg" style="border:3px double black;"/></td>
 <td align="center"><input type="submit" id="button" value="Отправить"/></td></tr>
