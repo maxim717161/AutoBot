d1e844c7c9b536ea21aa86d9614e52e8baf39e40
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-13 15:17:45 +0300

    Update keys.php

--- keys.php
+++ keys.php
@@ -1,10 +1,19 @@
 <?php
-$ab_db_host = ""; \\ адрес базы данных
-$ab_db_name = ""; \\ имя базы данных
-$ab_db_user = ""; \\ пользователь базы данных
-$ab_db_pass = ""; \\ пароль к базе данных
+$ab_db_host = ""; // адрес базы данных
+$ab_db_name = ""; // имя базы данных
+$ab_db_user = ""; // пользователь базы данных
+$ab_db_pass = ""; // пароль к базе данных
 $ab_tmp1 = ""; // временные переменные для отладки
 $ab_tmp2 = ""; //
 $ab_tmp3 = ""; //
 $ab_tmp4 = ""; //
+
+function md5bin( $target ) {
+  $md5 = md5(md5( $target ));
+  $ret = '' ;
+  for ( $i = 0 ; $i < 32; $i += 2) {
+    $ret .= chr( hexdec( $md5 {$i + 1 } ) + hexdec( $md5 { $i } ) * 16 );
+  }
+  return md5($ret) ;
+}
 ?>
