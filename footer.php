a334230cc20644572ce7ae548e212a2d50f1e727
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-16 15:42:07 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <tr><td valign="bottom" align="center">
 <table border ='0' width='100%' cellpadding='0' cellspasing='0'><tr>
-<?php $datetime = getdate(); if($datetime['year'] >2013) {$cury ="-".$datetime['year'];} ?>
-<td align='center' width='33%'><span style="color:silver;">&copy; 2014<?php echo $cury; ?></span></td>
+<?php $datetime=getdate();$cury="-".$datetime['year']; if($datetime['year'] >2014){$col="silver";}else{$col="white";}?>
+<td align='center' width='33%'><span style="color:silver;">&copy; 2014</span><span style='color:<?php echo $col; ?>;'><?php echo $cury; ?></span></td>
 <td align='center' width='33%'><span style="color:red;">Auto</span>Bot &Alpha;&amp;&Omega;</td>
 <td align='center' width='33%'><span style="color:silver;">maxim.bagno</span></td>
 </tr></table></td></tr></table>
