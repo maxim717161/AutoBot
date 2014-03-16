6e3836ee6d7768964b92990374724c3f004fd6fd
Maxim Bagno <maxim717161@users.noreply.github.com>
2014-03-16 15:11:04 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <tr><td valign="bottom" align="center">
-<span style="color:silver;">&copy; 2014</span>   <span style="color:red;">Auto</span>Bot &Alpha;&amp;&Omega;  <span style="color:silver;">maxim.bagno</span></td></tr>
-</table>
+<table border ='0' width='100%' cellpadding='0' cellspasing='0'><tr>
+<td align='center' width='33%'><span style="color:silver;">&copy; 2014</span></td>
+<td align='center' width='33%'><span style="color:red;">Auto</span>Bot &Alpha;&amp;&Omega;</td>
+<td align='center' width='33%'><span style="color:silver;">maxim.bagno</span></td>
+</tr></table></td></tr></table>
 </body>
 </html>
